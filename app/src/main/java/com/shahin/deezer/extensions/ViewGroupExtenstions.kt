b14571796eac9c248ca56123d48433520c9f9d5f
shahin68 <shahin.montazeri@gmail.com>
2020-12-02 20:39:48 +0330

    updated copyrights

--- app/src/main/java/com/shahin/deezer/extensions/ViewGroupExtenstions.kt
+++ app/src/main/java/com/shahin/deezer/extensions/ViewGroupExtenstions.kt
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2020. By Shahin Montazeri (shahin.montazeri@gmail.com
+ * Copyright (c) 2020. By Shahin Montazeri (shahin.montazeri@gmail.com) 
+ * Programmed for demonstration purposes
  */
 
 package com.shahin.deezer.extensions
